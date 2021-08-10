24dc8d4b6a61c2a281c46af51e2c742c6cd032b5
TharushiRachel <68675238+TharushiRachel@users.noreply.github.com>
2021-08-10 09:39:37 +0530

    Admin Post request error

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -3,13 +3,10 @@ package com.example.demo.controller;
 import com.example.demo.dto.AdminRequest;
 import com.example.demo.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "admin")
+
 public class AdminController {
 
 
@@ -17,7 +14,8 @@ public class AdminController {
     private AdminService adminService;
 
 
-    @PostMapping("/addAdmin")
+    @PostMapping("${app.endpoint.adminsSearch}")
+    @ResponseBody
     public void addAdmin(@RequestBody AdminRequest request) {
         adminService.save(request);
     }
--- src/main/java/com/example/demo/service/impl/AdminServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AdminServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.demo.entity.Admin;
 import com.example.demo.repository.AdminRepository;
 import com.example.demo.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 public class AdminServiceImpl implements AdminService {
@@ -13,10 +14,19 @@ public class AdminServiceImpl implements AdminService {
     private AdminRepository adminRepository;
 
     @Override
-    public Admin save(AdminRequest request) {
+    public Admin save( AdminRequest request) {
 
         Admin admin = new Admin();
         //set all values from request to this new admin object
+        admin.setNic(request.getNic());
+        admin.setFullName(request.getFullName());
+        admin.setEmail(request.getEmail());
+        admin.setPassword(request.getPassword());
+
+//        request.setNic(admin.getNic());
+//        request.setFullName(admin.getFullName());
+//        request.setEmail(admin.getEmail());
+//        request.setPassword(admin.getPassword());
         return adminRepository.save(admin);
     }
 }
