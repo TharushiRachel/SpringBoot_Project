b01c51ab8b30bb58efa38c9502bc6661b0ea6306
pasindu <pasindullakmal1012@gmail.com>
2021-08-17 11:51:50 +0530

    date time auto update added

--- src/main/java/com/example/config/ApplicationConfig.java
+++ src/main/java/com/example/config/ApplicationConfig.java
@@ -3,16 +3,24 @@ package com.example.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author Pasindu Lakmal
  */
 @Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class ApplicationConfig {
 
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper;
+    }
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new AuditorAwareImpl();
     }
 }
--- src/main/java/com/example/entity/Admin.java
+++ src/main/java/com/example/entity/Admin.java
@@ -2,13 +2,21 @@ package com.example.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
+import java.util.Date;
 import java.util.Set;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
 
 @Getter
 @Setter
 @Entity
 @Table(name="admin")
+@EntityListeners(AuditingEntityListener.class)
 public class Admin {
 
     @Id
@@ -32,6 +40,12 @@ public class Admin {
     @OneToMany(mappedBy = "admin", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Set<EmployeeLeave> employeeLeaves;
 
+    @CreatedDate
+    @Temporal(TIMESTAMP)
+    protected Date createdDate;
 
+    @LastModifiedDate
+    @Temporal(TIMESTAMP)
+    protected Date lastModifiedDate;
 
 }
